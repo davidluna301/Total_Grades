cfcb30a46e12cb2cdccbb8c34c8bcf9babb9ab98
David Alejandro Luna <david.lunamar@campusucc.edu.co>
2026-04-30 10:21:46 -0500

    Fix STATICFILES_STORAGE -> STORAGES for Django 6.x compatibility

--- evaluaciones_total_grades_estudiantes/settings.py
+++ evaluaciones_total_grades_estudiantes/settings.py
@@ -120,7 +120,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 LOGIN_URL = 'login'
 
